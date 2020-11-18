8b38cd2a1e7b9a0d0bd6a71156dc102066192dfb
Sénant Trévor <mouchet19@mbp-de-senant-1.home>
2020-11-18 21:02:21 +0100

    /hero route

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ app.get("/pick", async (req, res) => {
     const response = await axios.get(
       `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${api_key}&hash=${hash}&limit=100`
     );
-
     res.status(200).json({ data: response.data });
   } catch (error) {
     res.status().json({ error: error.message });
+  }
+});
+
+app.get("/hero", async (req, res) => {
+  console.log(req.query);
+  id = req.query.id;
+  try {
+    const response = await axios.get(
+      `http://gateway.marvel.com/v1/public/characters/${id}?ts=${timestamp}&apikey=${api_key}&hash=${hash}`
+    );
+    res.status(200).json({ data: response.data });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
