ccf30e5f9c889dfac435d233a98e9d4b5192467b
Sénant Trévor <mouchet19@mbp-de-senant.home>
2020-11-19 18:09:15 +0100

    variblaes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cors());
 
 require("dotenv").config();
 
-api_key = process.env.API_KEY;
-api_secret = process.env.API_SECRET;
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
 
 app.get("/pick", async (req, res) => {
   const date = new Date();
@@ -17,14 +17,15 @@ app.get("/pick", async (req, res) => {
 
   const hash = md5(timestamp + api_secret + api_key);
   console.log(req.query);
-  offset = req.query.skip;
+  const offset = req.query.skip;
   try {
     const response = await axios.get(
       `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${api_key}&hash=${hash}&limit=100&offset=${offset}`
     );
     res.status(200).json({ data: response.data });
   } catch (error) {
-    res.status(400).json({ error: error.response });
+    console.log(error.response);
+    res.status(400).json({ error: error.message });
   }
 });
 
@@ -34,7 +35,7 @@ app.get("/comics", async (req, res) => {
 
   const hash = md5(timestamp + api_secret + api_key);
   console.log(req.query);
-  offset = req.query.skip;
+  const offset = req.query.skip;
   try {
     const response = await axios.get(
       `http://gateway.marvel.com/v1/public/comics?orderBy=title&ts=${timestamp}&apikey=${api_key}&hash=${hash}&limit=100&offset=${offset}`
@@ -50,8 +51,8 @@ app.get("/research", async (req, res) => {
   const timestamp = Math.floor(date.getTime() / 1000);
 
   const hash = md5(timestamp + api_secret + api_key);
-  name = req.query.value;
-  offset = req.query.skip;
+  const name = req.query.value;
+  const offset = req.query.skip;
   console.log(name);
 
   try {
@@ -69,8 +70,8 @@ app.get("/search", async (req, res) => {
   const timestamp = Math.floor(date.getTime() / 1000);
 
   const hash = md5(timestamp + api_secret + api_key);
-  name = req.query.value;
-  offset = req.query.skip;
+  const name = req.query.value;
+  const offset = req.query.skip;
   console.log(name);
 
   try {
@@ -88,7 +89,7 @@ app.get("/hero", async (req, res) => {
   const timestamp = Math.floor(date.getTime() / 1000);
 
   const hash = md5(timestamp + api_secret + api_key);
-  id = req.query.id;
+  const id = req.query.id;
   try {
     const response = await axios.get(
       `http://gateway.marvel.com/v1/public/characters/${id}?ts=${timestamp}&apikey=${api_key}&hash=${hash}`
@@ -105,7 +106,7 @@ app.get("/hero/comics", async (req, res) => {
 
   const hash = md5(timestamp + api_secret + api_key);
   console.log(req.query);
-  id = req.query.id;
+  const id = req.query.id;
   try {
     const response = await axios.get(
       `http://gateway.marvel.com/v1/public/characters/${id}/comics?ts=${timestamp}&apikey=${api_key}&hash=${hash}`
